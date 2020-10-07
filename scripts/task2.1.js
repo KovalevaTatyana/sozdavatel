afb670375f97703026dce87f179b83a54df1c5e6
Tatyana <kovaleva.tatyana@protonmail.com>
2020-10-07 07:38:45 -0400

    end of task 2.1 except ajax

--- scripts/task2.1.js
+++ scripts/task2.1.js
@@ -1,1 +1,8 @@
 'use strict';
+
+function toggle() {
+    let toggle = document.getElementById("catalog");
+    toggle.classList.toggle("list");
+    let text = document.getElementById("btn_toggle");
+        text.innerHTML == 'Плитка' ? text.innerHTML = 'Список' : text.innerHTML = 'Плитка';
+}
